8d7acb0fc08212f8dff4ca954ff9457e707c7308
mohammad <69156294+mohammad-ashrafi2020@users.noreply.github.com>
2024-08-16 13:02:04 +0330

    pdf js

---
+++ pages/ahg2022ce-manuals.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <PDF src="/files/qr-code/AHG-2022-CE-Instruction-Manual.pdf" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PDF from "pdf-vue3";
+</script>
+
+<style>
+
+</style>
