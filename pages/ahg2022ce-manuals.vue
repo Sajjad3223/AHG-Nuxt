1749976bf9fb2e981ff9c5c1fbc4c341ee8ce891
mohammad <69156294+mohammad-ashrafi2020@users.noreply.github.com>
2024-08-16 13:08:13 +0330

    New Change

--- pages/ahg2022ce-manuals.vue
+++ pages/ahg2022ce-manuals.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="h-[85svh]">
+  <div >
     <PDF src="/files/qr-code/AHG-2022-CE-Instruction-Manual.pdf" pdfWidth="100%" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import PDF from "pdf-vue3";
+definePageMeta({
+  layout: 'empty'
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
